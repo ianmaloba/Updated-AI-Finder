44a9d60322731e4cc99b9684538cab744ba38651
Ian Maloba <ianmalobamwakha@gmail.com>
2024-09-12 17:05:00 +0300

    modified:   bestaifinder/bestaifinder/__pycache__/settings.cpython-312.pyc

--- bestaifinder/userauth/views.py
+++ bestaifinder/userauth/views.py
@@ -15,62 +15,49 @@ from django.db.models import Count
 from main.models import AITool, Bookmark
 from django.contrib.auth.decorators import login_required
 
-
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'password-reset/password_reset_form.html'
     email_template_name = 'password-reset/password_reset_email.html'
     subject_template_name = 'password-reset/password_reset_subject.txt'
     success_url = reverse_lazy('password_reset_done')
     
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+        """
+        Override the send_mail method to use our custom email sending logic.
+        """
+        current_site = get_current_site(self.request)
+        site_name = current_site.name
+        domain = current_site.domain
+        
+        context.update({
+            'domain': domain,
+            'site_name': site_name,
+            'protocol': 'https' if self.request.is_secure() else 'http',
+            'static_url': static('images/app-logo/logo2.png'),
+        })
+        
+        subject = render_to_string(subject_template_name, context)
+        subject = ''.join(subject.splitlines())
+        
+        html_message = render_to_string(email_template_name, context)
+        plain_message = strip_tags(html_message)
+        
+        send_mail(
+            subject,
+            plain_message,
+            from_email,
+            [to_email],
+            html_message=html_message,
+            fail_silently=False,
+        )
+
     def form_valid(self, form):
-        email = form.cleaned_data["email"]
-        user = self.get_user(email)
-        
-        if user:
-            current_site = get_current_site(self.request)
-            site_name = current_site.name
-            domain = current_site.domain
-            
-            context = {
-                'email': user.email,
-                'domain': domain,
-                'site_name': site_name,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'user': user,
-                'token': default_token_generator.make_token(user),
-                'protocol': 'https' if self.request.is_secure() else 'http',
-                'static_url': static('images/app-logo/logo2.png'),
-            }
-            
-            subject = render_to_string(self.subject_template_name, context)
-            subject = ''.join(subject.splitlines())
-            
-            html_message = render_to_string(self.email_template_name, context)
-            plain_message = strip_tags(html_message)
-            
-            send_mail(
-                subject,
-                plain_message,
-                settings.DEFAULT_FROM_EMAIL,
-                [user.email],
-                html_message=html_message,
-                fail_silently=False,
-            )
-        
+        """
+        Override form_valid to use the default password reset flow.
+        """
         return super().form_valid(form)
     
-    def get_user(self, email):
-        """
-        Given an email, return matching user who should receive a reset.
-        """
-        from django.contrib.auth import get_user_model
-        UserModel = get_user_model()
-        try:
-            return UserModel.objects.get(email=email)
-        except UserModel.DoesNotExist:
-            return None
-
-
 @login_required
 def user_dashboard(request):
     user_tools = AITool.objects.filter(user=request.user).order_by('-id')
